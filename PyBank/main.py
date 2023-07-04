e2c0e65c43242ac120b68a3da3295682232854ce
Mark Helotie <43053988+mark-helotie@users.noreply.github.com>
2023-07-04 14:57:39 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,69 +1,71 @@
 # PyBank --- Python Challenge
 
-# import modules os and csv
+# Import os and csv modules
 import os
 import csv
 
-# creating lists to store data
+# Initialize variables
 months = []
 profitloss = []
 
-# set path for the input file
-budgetcsv = os.path.join('Resources', 'budget_data.csv')
+# Set path for the input file
+input_path = os.path.join('Resources', 'budget_data.csv')
 
-# open the CSV
-with open(budgetcsv, newline="") as csvfile:
-    budgetreader = csv.reader(csvfile, delimiter=",")
+# Open the CSV
+with open(input_path, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-    # skip the header row
-    next(budgetreader)
+    # Skip the header row
+    next(csvreader)
 
-    # loop through the CSV
-    for row in budgetreader:
-        # add date
+    # Loop through the CSV
+    for row in csvreader:
+    
+        # Add date
         months.append(row[0])
-        # add Profit/Loss
+        
+        # Add Profit/Loss
         profitloss.append(float(row[1]))
 
-# calculate the total months
+# Calculate the total months
 totalmonths = (len(months))
 
-# calculate the net total amount
+# Calculate the net total amount
 totalamount = sum(profitloss)
 
-# calculate the average change per month
+# Calculate the average change per month
 avgchange = totalamount / totalmonths
 
-# calculate the greatest increase
+# Calculate the greatest increase
 maxprofit = max(profitloss)
 
-# use the index of the greatest increase to find the corresponding date
+# Use the index of the greatest increase to find the corresponding date
 indexmax = profitloss.index(maxprofit)
 maxmonth = months[indexmax]
 
-# calculate the greatest decrease 
+# Calculate the greatest decrease 
 minprofit = min(profitloss)
 
-# use the index of the greatest decrease to find the corresponding date
+# Use the index of the greatest decrease to find the corresponding date
 indexmin = profitloss.index(minprofit)
 minmonth = months[indexmin]
 
-# format the output accordingly
+# Format the output accordingly
 financialanalysis = (f'''
 Financial Analysis
 ----------------------------------
 Total Months: {totalmonths}
-Total: ${totalamount:.2f}
-Average Change: {avgchange:.2f}
-Greatest Increase in Profits: {maxmonth} ({maxprofit:.2f})
-Greatest Decrease in Profits: {minmonth} ({minprofit:.2f})
+Total: ${totalamount:,.0f}
+Average Change: ${avgchange:,.2f}
+Greatest Increase in Profits: {maxmonth} (${maxprofit:,.0f})
+Greatest Decrease in Profits: {minmonth} (${minprofit:,.0f})
 
 ''')
 
-# print out the analysis in the terminal
+# Print out the analysis in the terminal
 print(financialanalysis)
 
-# create a .txt file containing the same analysis
+# Create a .txt file containing the same analysis
 analysispath = os.path.join('analysis', 'financial_analysis.txt')
 with open(analysispath,'w') as textfile:
     textfile.write(financialanalysis)
